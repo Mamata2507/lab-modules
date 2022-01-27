5fcc09eb444491af32ba0bf2e024a67046dc6bff
fahdjamy <fahdjamy2@gmail.com>
2022-01-26 20:44:39 -0600

    add lab11-elibrary

---
+++ lab11-elibrary/src/test/java/edu/miu/cs425/demowebapps/elibrary/elibrary/lab11/Lab11ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs425.demowebapps.elibrary.elibrary.lab11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
